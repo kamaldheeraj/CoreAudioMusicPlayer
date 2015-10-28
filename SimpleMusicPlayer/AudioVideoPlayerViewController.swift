91b4fd24d90c04237066670572882215b72a4740
kamaldheeraj <Navya@Navya-MB-P.local>
2015-10-28 00:46:58 -0500

    Final Version .. Possible Cosmetic changes required.

--- SimpleMusicPlayer/AudioVideoPlayerViewController.swift
+++ SimpleMusicPlayer/AudioVideoPlayerViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 import AVFoundation
 import AVKit
 class AudioVideoPlayerViewController: UIViewController {
-
+    //var for UIImageView to display mp3 image created with a default image for mp3s with no images
     var mp3ImageView = UIImageView(image: UIImage(named: "Stock"))
-    @IBOutlet weak var songLabel: UINavigationItem!
+    var sliderImage = UIImage(named: "sliderImage")
+    
+    var timer:NSTimer?
+    
+    @IBOutlet weak var songVideoLabel: UINavigationItem!
     
     @IBOutlet weak var slider: UISlider!
+    
     @IBOutlet weak var playPauseButton: UIButton!
     
     var songPath:String!
@@ -22,7 +27,9 @@ class AudioVideoPlayerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //http://stackoverflow.com/questions/30243658/displaying-artwork-for-mp3-file
+        
+        //Start of Code to display mp3 images
+        //Reference: http://stackoverflow.com/questions/30243658/displaying-artwork-for-mp3-file
         let playerItem = AVPlayerItem( URL: NSURL(fileURLWithPath: songPath))
         let metaDataList = playerItem.asset.metadata as! [AVMetadataItem]
         for metaData in metaDataList{
@@ -41,14 +48,32 @@ class AudioVideoPlayerViewController: UIViewController {
         mp3ImageView.frame = CGRect(x: 15, y: 35, width: view.frame.width, height: view.frame.height-85)
         mp3ImageView.contentMode = UIViewContentMode.ScaleAspectFit
         view.addSubview(mp3ImageView)
-        songLabel.title = songPath.lastPathComponent.stringByDeletingPathExtension
+        //End of Code to display mp3 images
+        
+        slider.setThumbImage(sliderImage, forState: .Normal)
+        
+        songVideoLabel.title = songPath.lastPathComponent.stringByDeletingPathExtension
+        
+        //AVPlayer to play song/video
         player = AVPlayer(URL: NSURL(fileURLWithPath: songPath))
-                //slider.maximumValue = player
+        
+        //AVPlayerLayer to place video in view
         let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = CGRect(x: 15, y: 35, width: view.frame.size.width, height: view.frame.size.height-85)
         view.layer.addSublayer(playerLayer)
+        
+        let avAsset = AVURLAsset(URL: NSURL(fileURLWithPath: songPath)!, options: nil)
+        slider.maximumValue = Float(CMTimeGetSeconds(avAsset.duration))
+            
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "moveSlider", userInfo: nil, repeats: true)
+        
+        //Calling function to start playing
         myPlay()
 
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        timer?.invalidate()
     }
 
     override func didReceiveMemoryWarning() {
@@ -81,10 +106,16 @@ class AudioVideoPlayerViewController: UIViewController {
     }
     
     @IBAction func sliderValueChange(sender: AnyObject) {
-        //player.seekToTime(CMTimeMake(Int64(slider.value), 1))
-        println( slider.value )
+        player.seekToTime(CMTimeMake(Int64(slider.value), 1))
+        //println( slider.value )
         myPlay()
     }
+    
+    //Reference: https://www.youtube.com/watch?v=S3BSK8UVJyc
+    func moveSlider(){
+        slider.value = Float(CMTimeGetSeconds(player.currentTime()))
+    }
+    
     @IBAction func restartButtonPressed(sender: AnyObject) {
     player.seekToTime(CMTimeMake(0, 1))
         myPlay()
